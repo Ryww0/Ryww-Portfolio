156be84a181988f67743969f93383a46b5d5273f
Ryww0 <laurent.chaitas@gmail.com>
2022-09-23 11:54:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php require('./partials/header.php'); ?>
+
+<section class="d-flex justify-content-between p-3 homepage">
+    <div class="text-uppercase">
+        <h1>web developer</h1>
+        <h2>chaitas laurent</h2>
+        <h3>ウェブ開発者</h3>
+    </div>
+
+    <div class="d-flex align-items-center">
+        <img class="kyu" src="./assets/Images/kywbe_logo.png" alt="">
+    </div>
+
+    <div class="d-flex flex-column justify-content-between">
+        <div>
+            <h4>based in Lyon, France</h4>
+        </div>
+        <div>
+            <h5>00_</h5>
+            <div class="d-flex">
+                <h3>リユウ</h3>
+                <ul>
+                    <li>about</li>
+                    <li>work</li>
+                    <li>contact</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container mt500">
+    <div class="row col-12">
+        <div class="col-6 d-flex">
+            <div class="me-3">
+                <h3>わたし</h3>
+            </div>
+            <div>
+                <h2>Me, <br> myself <br> and I</h2>
+                <p>*This is a Quotes to modify*</p>
+            </div>
+        </div>
+        <div class="col-6 text-end">
+            <h6>いち</h6>
+        </div>
+    </div>
+    <div class="row col-12 pt-5 pb-5">
+        <div class="col-6 d-flex flex-column justify-content-between pe-5">
+            <p>
+                Lorem ipsum dolor sit amet. Et doloribus sunt ad blanditiis nihil qui praesentium voluptatem in doloremque quos rerum omnis sed corrupti mollitia. Est nobis dolores a enim At laudantium maiores in voluptatum dolore. Aut quod ratione est quaerat omnis rem eveniet aliquid 33 autem aliquid in adipisci quos aut ullam cupiditate.
+            </p>
+            <p>
+                Aut suscipit fugit sed modi voluptas ut iusto fuga. Ex libero porro et autem quibusdam aut repudiandae voluptate est recusandae placeat sed libero laudantium hic doloribus esse. Aut modi atque aut obcaecati ducimus ab voluptatum assumenda.
+            </p>
+            <ul class="d-flex justify-content-around ps-5 pe-5">
+                <li>Github</li>
+                <li>Instagram</li>
+                <li>LinkedIn</li>
+            </ul>
+        </div>
+        <div class="col-6 overflow-hidden h500 ps-5">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="container mt500">
+    <div class="row col-12">
+        <div class="col-6 d-flex">
+            <div class="me-3">
+                <h3>スキル</h3>
+            </div>
+            <div>
+                <h2>My <br> skills</h2>
+                <p>*This is a Quotes to modify*</p>
+            </div>
+        </div>
+        <div class="col-6 text-end">
+            <h6>に</h6>
+        </div>
+    </div>
+    <div class="row col-12 d-flex justify-content-between pt-5 pb-5">
+        <div class="d-flex col-3">
+            <div class="d-flex flex-row-reverse me-3">
+                <h2 class="card-skills-title">front-end</h2>
+                <h3>フロント エンド</h3>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet. Est eveniet aliquam cum eligendi molestias et veritatis illo est temporibus eveniet. Vel aspernatur iusto ut facilis sunt aut unde modi!
+            </p>
+        </div>
+        <div class="d-flex col-3">
+            <div class="d-flex flex-row-reverse me-3">
+                <h2 class="card-skills-title">back-end</h2>
+                <h3>バックエンド</h3>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet. Est eveniet aliquam cum eligendi molestias et veritatis illo est temporibus eveniet. Vel aspernatur iusto ut facilis sunt aut unde modi!
+            </p>
+        </div>
+        <div class="d-flex col-3">
+            <div class="d-flex flex-row-reverse me-3">
+                <h2 class="card-skills-title">graphism</h2>
+                <h3>グラフィックデザイン</h3>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet. Est eveniet aliquam cum eligendi molestias et veritatis illo est temporibus eveniet. Vel aspernatur iusto ut facilis sunt aut unde modi!
+            </p>
+        </div>
+    </div>
+</section>
+
+<section class="container mt500">
+    <div class="row col-12">
+        <div class="col-6 d-flex">
+            <div class="me-3">
+                <h3>プロジェクト</h3>
+            </div>
+            <div>
+                <h2>My <br> projects</h2>
+                <p>*This is a Quotes to modify*</p>
+            </div>
+        </div>
+        <div class="col-6 text-end">
+            <h6>さん</h6>
+        </div>
+    </div>
+    <div class="row col-12 d-flex justify-content-between pt-5 pb-5">
+    </div>
+</section>
+
+<div class="test"></div>
+<?php require('./partials/footer.php'); ?>

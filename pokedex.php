2d7886f25281e1a67bb417e599c0260a0c85ba7f
Ryww0 <laurent.chaitas@gmail.com>
2022-10-10 00:51:49 +0200

    pokedex done

--- pokedex.php
+++ pokedex.php
@@ -13,15 +13,20 @@
     <section class="row col-12 pt-5 pb-5">
         <div class="col-6 d-flex flex-column justify-content-between h500">
             <div>
-                <p>Qui nihil quos quo odio dolorem sit facere voluptate vel voluptates officia aut corrupti animi. Qui illo quae qui voluptas impedit eum tenetur officiis aut repellat quae. Est libero galisum ut sint corrupti aut corporis harum aut earum quasi in possimus perspiciatis.</p>
+                <p>
+                    I design a pokedex that find the pokemon you type using the keyboard.
+                    It shows the types of the pokemon and a litle description thanks to
+                    an API.
+                </p>
             </div>
             <div class="d-flex flex-column text-end">
+                <span><a href="https://ryww0.github.io/Pokedex/" target="_blank">https://ryww0.github.io/Pokedex/</a></span>
                 <span class="bt-work-page bb-work-page pt-2 pb-2">2022</span>
                 <span class="bb-work-page pt-2 pb-2">Javascript & CSS / API</span>
             </div>
         </div>
-        <div class="col-6 h500 ps-5">
-            <img class="h500 w1500" src="https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg" alt="">
+        <div class="col-6 h500 ps-5 scrolling-wrapper">
+            <img class="h500" src="./assets/Images/pokedex.png" alt="">
         </div>
     </section>
 </main>

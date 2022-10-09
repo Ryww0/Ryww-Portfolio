1cc84a2eb33eb981ca055fde20c38c11be005caa
Ryww0 <laurent.chaitas@gmail.com>
2022-10-10 00:33:02 +0200

    library done

--- library.php
+++ library.php
@@ -13,7 +13,12 @@
     <section class="row col-12 pt-5 pb-5">
         <div class="col-6 d-flex flex-column justify-content-between h500">
             <div>
-                <p>Qui nihil quos quo odio dolorem sit facere voluptate vel voluptates officia aut corrupti animi. Qui illo quae qui voluptas impedit eum tenetur officiis aut repellat quae. Est libero galisum ut sint corrupti aut corporis harum aut earum quasi in possimus perspiciatis.</p>
+                <p>
+                    The goal of this fictive project was to create a library management. Using
+                    a graphic interface, users should be able to create, read, modify and
+                    delete books, users and lease. I used PHP and object oriented programming
+                    to create this application.
+                </p>
             </div>
             <div class="d-flex flex-column text-end">
                 <span class="bt-work-page bb-work-page pt-2 pb-2">2022</span>
@@ -21,7 +26,7 @@
             </div>
         </div>
         <div class="col-6 h500 ps-5 scrolling-wrapper">
-            <img class="h500 w1500" src="./assets/Images/test.jpg" alt="">
+            <img class="h500" src="./assets/Images/library.png" alt="">
         </div>
     </section>
 </main>
